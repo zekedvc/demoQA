f5eefca5f97cf8a60160d9a7b4ab77dec1d87d29
Zeke <ezequiel.dellaechia@gmail.com>
2023-09-02 16:40:23 -0300

    Added first test example to check DEMOQA site title

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,13 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
+test("has title", async ({ page }) => {
+  await page.goto("https://demoqa.com/");
+
+  // Expect a title to contain a substring
+  await expect(page).toHaveTitle("DEMOQA");
+});
+
+/*
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
@@ -16,3 +24,4 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+*/
